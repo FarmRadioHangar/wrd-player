ca489c9f39eecdf23f7e8187552f71d15e620798
Johannes Hilden <johannes@isomorphic.co>
2017-02-08 11:26:34 +0300

    Add basic markup for audio player

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import AudioPlayer from 'react-responsive-audio-player';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>It Works!</h1>
-        <p>This React project just works including <span className="redBg">module</span> local styles.</p>
-        <p>Enjoy!</p>
+        <div>
+          <ul>
+            <li>Audio #1</li>
+            <li>Audio #2</li>
+            <li>Audio #3</li>
+            <li>Audio #4</li>
+          </ul>
+        </div>
+        <div style={{width: '100%', position: 'fixed', bottom: 0, left: 0}}>
+          <AudioPlayer playlist={[]} />
+        </div>
       </div>
     )
   }

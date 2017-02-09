6a06786dc3f9e2fcdd1bc51ee4ccffd4d9aea8dc
Johannes Hilden <johannes@isomorphic.co>
2017-02-09 11:43:51 +0300

    Return rows from Audio sheet

--- sheets/index.js
+++ sheets/index.js
@@ -1,27 +1,40 @@
 var express = require('express');
-var GoogleSpreadsheet = require("google-spreadsheet");
+var GoogleSpreadsheet = require('google-spreadsheet');
 var creds = require('./client_secret.json');
 
 var app = express();
-var api = new GoogleSpreadsheet('1Ho4S9BGaAGMEfXyIPfue_PazAV1qoUrWRM4s-L24i1A');
+var api = new GoogleSpreadsheet('166G5N1h02rYYVwKRUFmcHcOyY3UAXLZ0YPPukxfd7WA');
 
 api.useServiceAccountAuth(creds, function(err) {
   api.getInfo(function(err, info) {
-    console.log(info.title + ' is loaded.');
+    console.log(info.title + ' loaded.');
   });
 });
 
 app.get('/', function(req, res) {
   api.useServiceAccountAuth(creds, function(err) {
     api.getInfo(function(err, info) {
-
-      var sheet = info.worksheets[0];
+      var sheet = info.worksheets.filter(sheet => sheet.title === 'Audio');
+      if (sheet.length) {
+        sheet = sheet[0];
+      } else {
+        res.status(500).send('Worksheet \'Audio\' missing or inaccessible.')
+        return;
+      }
       sheet.getRows(function(err, rows) {
-        //
+        if (err) {
+          res.status(500).send('Error getting worksheet rows.')
+          return;
+        }
+        res.send(rows.map(row => ({
+          url: row.audiourl,
+          length: row.audiolength,
+          comments: row.audiocomments,
+          question: row.question,
+          datetime: row.datereceived,
+          row: row.rid
+        })));
       });
-
-      res.send(JSON.stringify(info));
-
     });
   });
 });

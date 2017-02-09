d81642523955ae76b4c5d350044f5f877fea3024
Johannes Hilden <johannes@isomorphic.co>
2017-02-09 11:55:07 +0300

    Enable CORS for express

--- sheets/index.js
+++ sheets/index.js
@@ -5,6 +5,12 @@ var creds = require('./client_secret.json');
 var app = express();
 var api = new GoogleSpreadsheet('166G5N1h02rYYVwKRUFmcHcOyY3UAXLZ0YPPukxfd7WA');
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 api.useServiceAccountAuth(creds, function(err) {
   api.getInfo(function(err, info) {
     console.log(info.title + ' loaded.');

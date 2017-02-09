75f2788aaf09c8857a992a2cf78cf70854fb559e
Johannes Hilden <johannes@isomorphic.co>
2017-02-09 16:24:20 +0300

    Rewrite everything

--- src/app.jsx
+++ src/app.jsx
@@ -1,78 +1,127 @@
-import React from 'react';
-import AudioPlayer from 'react-responsive-audio-player';
-import { Router, Route, Link, browserHistory } from 'react-router';
-import $ from 'jquery'
-
-// ----------- test data -----------
-const clips = [
-  {
-    id: '1',
-    country: 'tz',
-    url: '',
-    length: '',
-    gender: 'M',
-    ageGroup: '12-25'
-  },
-  {
-    id: '2',
-    country: 'ke',
-    url: '',
-    length: '',
-    gender: 'F',
-    ageGroup: '25-35'
-  },
-  {
-    id: '3',
-    country: 'ug',
-    url: '',
-    length: '',
-    gender: 'F',
-    ageGroup: '35-55'
-  }
-];
-
-const List = (props) =>
-  <div>
-    <h1><Link to='/'>Messages</Link></h1>
-    <ul>
-      {props.route.messages.map(message => 
-        <li key={message.id}>
-          <p>
-            {message.country} {message.gender} <Link to={`/audio/${message.id}`}>Play</Link>
-          </p>
-        </li>
-      )}
-    </ul>
-    {props.children}
-  </div>
-
-const Player = (props) => 
-  <div style={{width: '100%', position: 'fixed', bottom: 0, left: 0}}>
-    <Link to='/'>Close msg {props.params.id}</Link>
-    <AudioPlayer playlist={[]} />
-  </div>
-
-function fetchMessages() {
-  const opts = {crossDomain: true};
-  $.get('http://localhost:3000', opts, (data) => {
-    //console.log('-------------');
-    //console.log(data);
-  });
-}
+import React  from 'react';
+import $      from 'jquery'
+import Player from 'react-audio-player';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      selected: null,
+      error: false,
+      fetching: false,
+      playerMode: null        // 'playing' | 'buffering' | 'error'
+    };
+    window.addEventListener(
+      'hashchange', 
+      this.updateSelected.bind(this), 
+      false
+    );
+  }
+  fetchMessages() {
+    this.setState({fetching: true});
+    $.ajax({
+      url: 'http://localhost:3000',
+      type: 'GET',
+      crossDomain: true,
+      success: (messages) => { 
+        this.setState({messages});
+        this.updateSelected(null, messages);
+      },
+      error: () => { console.error('Server unavailable.') },
+      complete: () => this.setState({fetching: false})
+    });
+  }
+  updateSelected(ev, messages) {
+    if (!messages) {
+      messages = this.state.messages;
+    }
+    const location = window.location.hash.replace(/^#\/?|\/$/g, '').split('/');
+    if (location && location[0] === 'messages') {
+      const message = messages[location[1]]; 
+      if (message) {
+        if (!this.state.selected || this.state.selected.url != message.url) {
+          this.setState({playerMode: 'buffering'});
+        }
+        this.setState({
+          selected: message, 
+          error: false
+        });
+      } else {
+        this.setState({selected: null, error: true});
+      }
+    } 
+  }
+  componentDidMount() {
+    this.fetchMessages();
+  }
   render() {
+    const { 
+      connected, 
+      error,
+      fetching, 
+      messages, 
+      playerMode, 
+      selected
+    } = this.state;
     return (
       <div>
-        <button onClick={fetchMessages}>
-          Load messages
-        </button>
-        <Router history={browserHistory}>
-          <Route path='/' component={List} messages={clips}>
-            <Route path='audio/:id' component={Player} />
-          </Route>
-        </Router>
+        {fetching ? (
+          <div>
+            Fetching messages&hellip;
+          </div>
+        ) : (
+          <div>
+            <h1><a href='/#'>App</a></h1>
+            <button onClick={() => this.fetchMessages()}>
+              Load messages
+            </button>
+            <ul>
+              {messages.map((message, i) => 
+                <li key={i}>
+                  <a href={`/#/messages/${i}`}>Link</a>
+                  {JSON.stringify(message)}
+                </li>
+              )}
+            </ul>
+            {error ? (
+              <div>
+                Sorry, that message wasn't found. 
+              </div>
+            ) : (
+              selected && (
+                <div>
+                  mode: {playerMode || 'playing'}
+                  {'error' == playerMode ? (
+                    <div>
+                      Unable to play audio. Perhaps this message is made of cheese.
+                    </div>
+                  ) : (
+                    <div>
+                      <p>
+                        {''+selected.url}
+                      </p>
+                      <div style={{margin: 0, padding: 0, position: 'fixed', left: 0, bottom: 0}}>
+                        {'buffering' == playerMode && (
+                          <div>
+                            Buffering&hellip;
+                          </div>
+                        )}
+                        <Player
+                          src       = {selected.url} 
+                          onCanPlay = {() => this.setState({playerMode: 'playing'})}
+                          onError   = {() => this.setState({playerMode: 'error'})}
+                          autoPlay
+                        />
+                      </div>
+                    </div>
+                  )}
+                </div>
+              )
+            )}
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }

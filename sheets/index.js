41a26c347b626ccae66effb8a83cbf93bc0492f4
Johannes Hilden <johannes@isomorphic.co>
2017-02-09 10:56:18 +0300

    Add basic express app boilerplate

--- sheets/index.js
+++ sheets/index.js
@@ -3,14 +3,29 @@ var GoogleSpreadsheet = require("google-spreadsheet");
 var creds = require('./client_secret.json');
 
 var app = express();
-var sheet = new GoogleSpreadsheet('1Ho4S9BGaAGMEfXyIPfue_PazAV1qoUrWRM4s-L24i1A');
+var api = new GoogleSpreadsheet('1Ho4S9BGaAGMEfXyIPfue_PazAV1qoUrWRM4s-L24i1A');
 
-sheet.useServiceAccountAuth(creds, function(err) {
-  sheet.getInfo(function(err, info) {
+api.useServiceAccountAuth(creds, function(err) {
+  api.getInfo(function(err, info) {
     console.log(info.title + ' is loaded.');
   });
 });
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  api.useServiceAccountAuth(creds, function(err) {
+    api.getInfo(function(err, info) {
+
+      var sheet = info.worksheets[0];
+      sheet.getRows(function(err, rows) {
+        //
+      });
+
+      res.send(JSON.stringify(info));
+
+    });
+  });
+});
+
+app.listen(3000, function() {
+  console.log('App listening on port 3000.');
 });
